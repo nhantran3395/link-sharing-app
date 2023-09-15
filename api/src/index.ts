9c7b516c2d6c05742e710012ec8235113885a669
nhantran3395 <thanhnhan33.95@gmail.com>
2023-09-15 14:55:52 -0600

    chore: set up prettier and npm format script

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
 
-import * as process from "node:process";
+import * as process from 'node:process';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(morgan('short'));
 
-app.get('/ping',(req, res) => {
-    res.json({
-        message: "pong"
-    });
+app.get('/ping', (req, res) => {
+	res.json({
+		message: 'pong',
+	});
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+	console.log(`Server is running on port ${port}`);
+});
